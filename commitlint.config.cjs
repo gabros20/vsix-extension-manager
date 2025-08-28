20a64033320588faac2a35f84c0d69f78ec889c8
gabros20 <t.gabor880312@gmail.com>
2025-08-28 20:22:13 +0200

    chore: increase commitlint body line length limit to 400 characters
    Increases the maximum line length for commit message body from 200 to 400 characters to accommodate longer descriptions. Also renames bulk-download-example.json to extensions.json and adds plugins.json.

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
     "header-max-length": [2, "always", 200],
-    "body-max-line-length": [2, "always", 200],
+    "body-max-line-length": [2, "always", 400],
     "footer-max-line-length": [2, "always", 200],
   },
 };
